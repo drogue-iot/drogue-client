8cd8b719db3ededf16b74a7350ead8dfe430e26a
Jens Reimann <jreimann@redhat.com>
2021-07-30 14:44:27 +0200

    All or nothing. Better define the kafka status.

--- src/registry/v1/data/app/kafka.rs
+++ src/registry/v1/data/app/kafka.rs
@@ -7,17 +7,20 @@ use std::collections::HashMap;
 pub struct KafkaAppStatus {
     pub observed_generation: u64,
     pub conditions: core::v1::Conditions,
+    /// An explicit Kafka downstream status.
+    ///
+    /// This may be provided by the system to direct the downstream events (device-to-cloud) to an
+    /// alternate Kafka target. If provided, this must contain both the server as well as the topic.
+    /// Most likely, access credentials have to be provided too.
+    #[serde(default)]
     pub downstream: Option<KafkaDownstreamStatus>,
 }
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct KafkaDownstreamStatus {
-    #[serde(default, skip_serializing_if = "String::is_empty")]
     pub topic: String,
-    #[serde(default, skip_serializing_if = "String::is_empty")]
     pub bootstrap_servers: String,
-    #[serde(default, skip_serializing_if = "HashMap::is_empty")]
     pub properties: HashMap<String, String>,
 }
 
